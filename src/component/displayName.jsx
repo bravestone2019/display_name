ffd52cba58461b029cff826e1919dc8e3c93fa7f
Animesh dung dung <animeshdungdung9@gmail.com>
2024-08-04 21:59:42 +0530

    correcting the structure of the page

--- src/component/displayName.jsx
+++ src/component/displayName.jsx
@@ -2,9 +2,34 @@ function displayName () {
 return (
     <div>
         <h2>Full Name Display</h2>
-        <h3>First Name:<input/> </h3>
-        <h3>Last Name:<input/></h3>
-        <button >Submit</button>
+        {/* <h3><span>First Name:<input/></span></h3>
+        <span><h3>Last Name:<input/></h3></span>
+        <button>Submit</button> */}
+         <form>
+        <div>
+          <label>
+            First Name:
+            <input 
+              type="text" 
+            //   value={firstName} 
+            //   onChange={(e) => setFirstName(e.target.value)} 
+            />
+          </label>
+        </div>
+        <div style={{ marginTop: '0px' }}>
+          <label>
+            Last Name:
+            <input 
+              type="text" 
+            //   value={lastName} 
+            //   onChange={(e) => setLastName(e.target.value)} 
+            />
+          </label>
+        </div>
+        <div style={{ marginTop: '0px' }}>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
 );
 }
